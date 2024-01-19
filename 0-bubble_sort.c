97df1cb41c0def371eb4aca39deca2e7edb5586f
saadsgh123 <sdsghouri@gmail.com>
2024-01-19 22:59:37 +0100

    task 0 try 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+/*
+ * File: 0-bubble-sort.c
+ * Authors: Aziza CHBIHI & Saad SGHOURI
+ */
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order.
+ * @array: list to iterate
+ * @size: number of elements in the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, clean = 1, temp = 0;
+
+	for (i = 0; i < size - 1; ++i)
+	{
+		for (j = 0; j < size - i - 1; ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				clean = 0;
+			}
+		}
+	if (clean)
+		break;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,4 +15,10 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/*----------------functions definition------------------*/
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+void print_list(const listint_t *list);
 #endif
