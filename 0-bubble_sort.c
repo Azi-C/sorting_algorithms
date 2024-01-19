3fb712f0b71536f5217c2f32089329bdedea83fc
saadsgh123 <sdsghouri@gmail.com>
2024-01-19 23:25:24 +0100

    task 0 try 5 (compilation error solved)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,12 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, clean = 1, temp = 0;
+	int clean = 1, temp;
+	size_t i, j, len = size;
 
-	for (i = 0; i < size - 1; ++i)
+	for (i = 0; i < len - 1; ++i)
 	{
-		for (j = 0; j < size - 1; ++j)
+		for (j = 0; j < len - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
