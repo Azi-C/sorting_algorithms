b82892adfeb4404e14b1da7fd312ec2722541e08
saadsgh123 <sdsghouri@gmail.com>
2024-01-22 01:40:47 +0100

    task 1 try 1

--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 /*----------------functions definition------------------*/
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-
 void print_list(const listint_t *list);
+void swap_nodes(listint_t **head, listint_t **n1, listint_t *n2);
+void insertion_sort_list(listint_t **list);
 #endif
